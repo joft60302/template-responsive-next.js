e70d4b076df393eaab38c4c09e39f2e5c78ed354
joft60302 <51731096+joft60302@users.noreply.github.com>
2023-09-27 14:21:45 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Layout from './components/layout';
-import { Box } from '@mui/material';
+
 
 function index() {
   return (
     <div>
-      <Layout
-         nav={<Box>Navbar</Box>}
-         Sidebar={<Box>Sidebar</Box>}
-         Main={<Box>Main</Box>}
-         Content1={<Box>Content1</Box>}
-         Content2={<Box>Content2</Box>}
-         Content3={<Box>Content3</Box>}
-         footer={<Box>Footer</Box>}
+      <Layout conheight="100vh" gridtemcol="1fr 1fr 1fr 1fr" gridtemrow="0.2fr 1.5fr 1.2fr 0.8fr" gridtemareas="'nav nav nav nav' 'sidebar main main main' 'sidebar content1 content2 content3' 'sidebar footer footer footer'"
+      mgridTemplateRows="0.4fr 0.4fr 2.2fr 1.2fr 1.2fr 1.2fr 1fr" mtemplateareas="'nav' 'sidebar' 'main' 'content1' 'content2' 'content3' 'footer'"  bgimage="url('')" 
+         nav={<>Nav</>}
+         Sidebar={<>Sidebar</>}
+         Main={<>main</>}
+         Content1={<>content1</>}
+         Content2={<>content2</>}
+         Content3={<>content3</>}
+         footer={<>Footer</>}
       />
     </div>
   )
